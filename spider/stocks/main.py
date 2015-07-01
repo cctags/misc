395a81bed5fa52bc342f4342b65e788daaedc399
ctags <sherking@gmail.com>
2015-07-01 12:37:27 +0800

    Added the simple stock viewer.

---
+++ spider/stocks/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python -B
+
+import argparse
+import colorama
+
+import sina
+
+program_version = "v0.0.1"
+
+def print_line(line, color=False):
+    c0 = colorama.Fore.RESET
+    if color:
+        if "+" in line[5]:
+            c0 = colorama.Fore.RED
+        elif "-" in line[5]:
+            c0 = colorama.Fore.GREEN
+
+    c1 = colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.RESET_ALL
+
+    format_string = "%-8s\t%-20s\t%8s\t" + c0 + "%8s\t%8s\t%s" + c1
+
+    print(format_string % (line[0], line[1].decode("utf-8"), line[2], \
+          line[3], line[4], line[5]))
+
+def main():
+    # Parse the command line options
+    parser = argparse.ArgumentParser(description="Simple stock viewer. %s" % (program_version))
+    parser.add_argument("-t", "--type", help="specify the type", choices=("sh", "fund"))
+    parser.set_defaults(type="sh")
+    parser.add_argument("-c", "--color", help="highlight the output (default off)", action="store_true")
+    parser.add_argument("-v", "--version", action="version", version=program_version)
+    parser.add_argument("code", nargs=1, help="specify the code")
+    args = parser.parse_args()
+
+    if args.type == "sh":
+        f = sina.get_sh
+    elif args.type == "fund":
+        f = sina.get_fund
+    else:
+        f = None
+
+    assert(f is not None)
+
+    t = f(args.code[0])
+    print_line(t, args.color)
+
+if __name__ == "__main__":
+    main()
---
+++ spider/stocks/sina.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+
+import cStringIO
+import HTMLParser
+import pycurl
+
+class MyFundHTMLParse(HTMLParser.HTMLParser):
+
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+
+        self.span = False
+        self.span_fund_code = False
+        self.span_fund_name = False
+        self.span_premium_rate = False
+        self.span_fund_value = False
+        self.span_fund_valRan = False
+        self.span_fund_valExt = False
+
+        self.result = ["name",  "code",   "premium_rate",
+                       "value", "valRan", "valExt"]
+
+    def _check_attribute(self, attr, attrs):
+        for i in attrs:
+            if i[0] == "class" and attr in i[1].split():
+                return True
+        return False
+
+    def _set_result(self, index, data):
+        self.result[index] = data.strip()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "span":
+            self.span = True
+            if self._check_attribute("fund_code", attrs):
+                self.span_fund_code = True
+            if self._check_attribute("fund_name", attrs):
+                self.span_fund_name = True
+            if self._check_attribute("premium_rate", attrs):
+                self.span_premium_rate = True
+            if self._check_attribute("fund_value", attrs):
+                self.span_fund_value = True
+            if self._check_attribute("fund_valRan", attrs):
+                self.span_fund_valRan = True
+            if self._check_attribute("fund_valExt", attrs):
+                self.span_fund_valExt = True
+
+    def handle_endtag(self, tag):
+        if self.span:
+            if tag == "span":
+                self.span = False
+                self.span_fund_code = False
+                self.span_fund_name = False
+                self.span_premium_rate = False
+                self.span_fund_value = False
+                self.span_fund_valRan = False
+                self.span_fund_valExt = False
+
+    def handle_data(self, data):
+        if self.span:
+            if self.span_fund_code:
+                self._set_result(0, data)
+            if self.span_fund_name:
+                self._set_result(1, data)
+            if self.span_premium_rate:
+                self._set_result(2, data)
+            if self.span_fund_value:
+                self._set_result(3, data)
+            if self.span_fund_valRan:
+                self._set_result(4, data)
+            if self.span_fund_valExt:
+                self._set_result(5, data)
+
+class MyShHTMLParse(HTMLParser.HTMLParser):
+
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+
+        self.h1 = False
+        self.h1_j_stockName = False
+        self.h1_j_stockName_strong = False
+
+        self.span = False
+        self.span_j_serialNum = False
+        self.span_j_valRange = False
+        self.span_j_valExtent = False
+
+        self.strong = False
+        self.strong_j_stockCurValue = False
+
+        self.result = ["name",  "code",   "premium_rate",
+                       "value", "valRan", "valExt"]
+
+    def _check_attribute(self, attr, attrs):
+        for i in attrs:
+            if i[0] == "class" and attr in i[1].split():
+                return True
+        return False
+
+    def _set_result(self, index, data):
+        self.result[index] = data.strip()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "h1":
+            self.h1 = True
+
+            if ("class", "j_stockName") in attrs:
+                self.h1_j_stockName = True
+
+        elif tag == "span":
+            self.span = True
+
+            if ("class", "j_serialNum") in attrs:
+                self.span_j_serialNum = True
+
+            if self._check_attribute("j_valRange", attrs):
+                self.span_j_valRange = True
+
+            if self._check_attribute("j_valExtent", attrs):
+                self.span_j_valExtent = True
+
+        elif tag == "strong":
+            self.strong = True
+
+            if ("class", "j_stockCurValue") in attrs:
+                self.strong_j_stockCurValue = True
+
+            if self.h1 and self.h1_j_stockName:
+                self.h1_j_stockName_strong = True
+
+    def handle_endtag(self, tag):
+        if self.h1:
+            if tag == "h1":
+                self.h1 = False
+                self.h1_j_stockName = False
+                self.h1_j_stockName_strong = False
+
+        if self.span:
+            if tag == "span":
+                self.span = False
+
+                self.span_j_serialNum = False
+                self.span_j_valRange = False
+                self.span_j_valExtent = False
+
+        if self.strong:
+            if tag == "strong":
+                self.strong = False
+                self.strong_j_stockCurValue = False
+
+                self.h1_j_stockName_strong = False
+
+    def handle_data(self, data):
+        if self.h1:
+            if self.h1_j_stockName:
+                if self.h1_j_stockName_strong:
+                    self._set_result(1, data)
+
+        if self.span:
+            if self.span_j_serialNum:
+                self._set_result(0, data)
+            if self.span_j_valRange:
+                self._set_result(4, data)
+            if self.span_j_valExtent:
+                self._set_result(5, data)
+
+        if self.strong:
+            if self.strong_j_stockCurValue:
+                self._set_result(3, data)
+
+def get_fund(code):
+
+    # Create a buffer object.
+    buf = cStringIO.StringIO()
+
+    # Get the fund data.
+    c = pycurl.Curl()
+    c.setopt(c.URL, r"http://stocks.sina.cn/fund/?code=%s&vt=4" % (code))
+    c.setopt(c.WRITEFUNCTION, buf.write)
+    c.setopt(c.CONNECTTIMEOUT, 20)
+    c.setopt(c.TIMEOUT, 20)
+    c.perform()
+
+    # Parse the data.
+    p = MyFundHTMLParse()
+    p.feed(buf.getvalue())
+    ret = p.result
+    p.close()
+
+    # Now close the buffer object.
+    buf.close()
+
+    # Return the value
+    return ret
+
+def get_sh(code):
+
+    # Create a buffer object.
+    buf = cStringIO.StringIO()
+
+    # Get the fund data.
+    c = pycurl.Curl()
+    c.setopt(c.URL, r"http://stocks.sina.cn/sh/?code=%s&vt=4" % (code))
+    c.setopt(c.WRITEFUNCTION, buf.write)
+    c.setopt(c.CONNECTTIMEOUT, 20)
+    c.setopt(c.TIMEOUT, 20)
+    c.perform()
+
+    # Parse the data.
+    p = MyShHTMLParse()
+    p.feed(buf.getvalue())
+    ret = p.result
+    p.close()
+
+    # Now close the buffer object.
+    buf.close()
+
+    # Calculate the previous value
+    ret[2] = "%.2f" % (float(ret[3]) - float(ret[4]))
+
+    # Return the value
+    return ret

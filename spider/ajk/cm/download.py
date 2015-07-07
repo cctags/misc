c0f3880f8abdd867878c023fdd054c76079394f6
ctags <sherking@gmail.com>
2015-07-07 13:15:26 +0800

    Update the download module.

--- spider/ajk/cm/download.py
+++ spider/ajk/cm/download.py
@@ -5,23 +5,30 @@ import os
 import re
 import subprocess
 
-url=r"http://www.sample.com/sub"
+url=r"http://www1.sample.com/sub"
 
+# NOTE: until now we have to run this on win32 machine.
 def download_p():
-    for i in range(1, 197):
-        u = r"wget %s/cm/p%d/ -O p/p%08d.html" % (url, i, i)
-        p = subprocess.Popen(u)
+    for i in range(1, 3234):
+        u = r"wget -c -nc -t 0 %s/W0QQpZ%d -O p/p%08d.html" % (url, i, i)
+        p = subprocess.Popen(u.split())
         p.wait()
 
 def parse_cm_from_p():
     com = []
 
-    for i in range(1, 197):
+    for i in range(1, 3234):
+
+        print("Parsing %d / 3233 .." % (i))
+
         filename = r"p/p%08d.html" % (i)
         with open (filename, "r") as myfile:
             data = myfile.read()
-        m = re.findall(r"%s/cm\d+" % (url), data)
-        com.extend(m)
+        m = re.findall(r"%s/view/\d+" % (url), data)
+
+        for t in m:
+            if t not in com:
+                com.append(t)
 
     p = open(r"p/p.txt", "wb")
     for i in com:

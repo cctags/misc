0a2a59cb9a217c064fc698aa750103b120ad665b
ctags <sherking@gmail.com>
2015-07-31 14:11:06 +0800

    Added the quiet flag.

--- tools/rename/main.py
+++ tools/rename/main.py
@@ -9,9 +9,9 @@ import sys
 
 def _parse_args():
     parser = argparse.ArgumentParser(description="Renames multiple files.")
-    parser.add_argument("-v", "--verbose", action="store_true", help="print names of files successfully renamed")
-    parser.add_argument("-n", "--no-act", action="store_true", help="show what files would have been renamed")
     parser.add_argument("-f", "--force", action="store_true", help="overwrite existing files")
+    parser.add_argument("-n", "--no-act", action="store_true", help="show what files would have been renamed")
+    parser.add_argument("-q", "--quiet", action="store_true", help="do not write anything to standard output")
     parser.add_argument("pattern", nargs=1)
     parser.add_argument("repl", nargs=1)
     parser.add_argument("file", nargs="+")
@@ -21,6 +21,15 @@ def _panic(message, errno=-1):
     print os.linesep + "*ERROR*: " + message
     sys.exit(errno)
 
+def _print_file_change(old_files, new_files, print_flags=None):
+
+    if print_flags is None:
+        print_flags = [True] * len(old_files)
+
+    for old, new, flag in zip(old_files, new_files, print_flags):
+        if flag:
+            print "'%s' -> '%s'" % (old, new)
+
 def main():
 
     # Parse the arguments
@@ -29,14 +38,13 @@ def main():
     pattern = args.pattern[0]
     repl = args.repl[0]
 
-    overwritten = False
-
     # The pattern and the repl can't be same
     if pattern == repl:
         _panic("'pattern' and 'repl' are same.")
 
     old_files = []
     new_files = []
+    overwritten_flags = []
 
     # Find all the files
     for pathname in args.file:
@@ -59,25 +67,39 @@ def main():
 
         # overwritten?
         if new in new_files:
-            overwritten = True
+            overwritten_flags[new_files.index(new)] = True
+            overwritten_flags.append(True)
+        else:
+            overwritten_flags.append(False)
 
         # Update the list
         new_files.append(new)
 
+    # no-act
+    if args.no_act:
+        _print_file_change(old_files, new_files)
+        sys.exit(0)
+
     # Check if overwritten
     if not args.force:
-        if overwritten:
-            panic("overwritten.")
+        if True in overwritten_flags:
+            _print_file_change(old_files, new_files, overwritten_flags)
+            _panic("overwritten.")
 
     # Loop all the files
+    change_flags = []
+
     for old, new in zip(old_files, new_files):
 
-        if args.verbose or args.no_act:
-            print "'%s' -> '%s'" % (old, new)
-
         if old != new:
-            if not args.no_act:
-                os.rename(old, new)
+            os.rename(old, new)
+            change_flags.append(True)
+        else:
+            change_flags.append(False)
+
+    if not args.quiet:
+        _print_file_change(old_files, new_files, change_flags)
+        print "\n%d file(s) have been renamed." % (change_flags.count(True))
 
 if __name__ == "__main__":
     main()
